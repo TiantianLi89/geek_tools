53b125f840be3a04648a866bd89b84601433660b
武夷 <ben.wangz@alibaba-inc.com>
2019-02-23 10:18:40 +0800

    #3 optimize the search process with the relation in the data

--- tree/src/main/java/tech/geekcity/open/geek/tools/tree/TreeConstructor.java
+++ tree/src/main/java/tech/geekcity/open/geek/tools/tree/TreeConstructor.java
@@ -1,8 +1,12 @@
 package tech.geekcity.open.geek.tools.tree;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import java.io.Serializable;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author ben.wangz
@@ -20,7 +24,7 @@ public class TreeConstructor<DataType extends Serializable> implements Serializa
          *
          * @param parent the parent node to test
          * @param child  the child node to test
-         * @return true if <code>parent</code> node is the parent of <code>child</code> node; otherwise false
+         * @return true if {@code parent} node is the parent of {@code child} node; otherwise false
          */
         boolean isParent(Node<DataType> parent, Node<DataType> child);
 
@@ -30,8 +34,8 @@ public class TreeConstructor<DataType extends Serializable> implements Serializa
          * </p>
          * <p>
          * this method should satisfy that
-         * <code>anotherNode</code> should be equal to <code>node</code>
-         * if and only if <code>node</code>equals to <code>anotherNode</code>
+         * {@code anotherNode} should be equal to {@code node}
+         * if and only if {@code node} equals to {@code anotherNode}
          * </p>
          *
          * @param node        one node
@@ -45,7 +49,7 @@ public class TreeConstructor<DataType extends Serializable> implements Serializa
          *
          * @param node  any node
          * @param other anther node
-         * @return the <code>RelationShip</code> between two input nodes
+         * @return the {@code RelationShip} between two input nodes
          */
         default RelationShip relationShip(
                 Node<DataType> node,
@@ -66,87 +70,211 @@ public class TreeConstructor<DataType extends Serializable> implements Serializa
         }
     }
 
+    private static final EnumSet<NodeComparator.RelationShip> LINKED_RELATION_SHIP
+            = EnumSet.of(NodeComparator.RelationShip.PARENT, NodeComparator.RelationShip.CHILD);
     private final NodeComparator<DataType> nodeComparator;
-    private final List<List<Node<DataType>>> nodeListPool = new ArrayList<>();
+    // format id -> {location, node}
+    private final Map<String, Pair<Long, Node<DataType>>> linkableLocation4IdKeyed = new HashMap<>();
+    // format parent -> List<{location, node}>
+    private final Map<String, List<Pair<Long, Node<DataType>>>> linkableLocation4ParentKeyed = new HashMap<>();
+    // format List<{location, node}
+    private List<Pair<Long, Node<DataType>>> notLinkableDataNodeLocation = new ArrayList<>();
+    private final AtomicLong locationCounter = new AtomicLong(0L);
 
     public TreeConstructor(NodeComparator<DataType> nodeComparator) {
         this.nodeComparator = nodeComparator;
     }
 
     public void clear() {
-        nodeListPool.clear();
+        linkableLocation4IdKeyed.clear();
+        linkableLocation4ParentKeyed.clear();
+        notLinkableDataNodeLocation.clear();
     }
 
-    public void add(Node<DataType> nodeAdded) {
-        if (nodeListPool.stream()
-                .flatMap(Collection::stream)
-                .anyMatch(node -> nodeComparator.equals(node, nodeAdded))) {
+    public void add(Node<DataType> nodeToAdd) {
+        add(nodeToAdd, true);
+    }
+
+    /**
+     * <p>
+     * if linkableDataTypeOptimize and {@code nodeToAdd} instanceof {@link LinkableDataType},
+     * the constructor will use the optimized method to add node
+     * </p>
+     *
+     * @param nodeToAdd                node to add
+     * @param linkableDataTypeOptimize turn on optimization or not
+     * @see LinkableDataType
+     */
+    public void add(Node<DataType> nodeToAdd, boolean linkableDataTypeOptimize) {
+        // omit any equal node
+        if (Stream
+                .concat(
+                        linkableLocation4IdKeyed
+                                .entrySet()
+                                .stream()
+                                .map(entry -> entry.getValue().getRight()),
+                        notLinkableDataNodeLocation
+                                .stream()
+                                .map(Pair::getRight))
+                .anyMatch(node -> nodeComparator.equals(node, nodeToAdd))) {
             return;
         }
-        List<List<Node<DataType>>> matchedNodeLists = nodeListPool.stream()
-                .filter(nodeList
-                        -> nodeList.stream()
-                        .map(node -> nodeComparator.relationShip(nodeAdded, node))
-                        .anyMatch(relationShip -> EnumSet
-                                .of(NodeComparator.RelationShip.PARENT, NodeComparator.RelationShip.CHILD)
-                                .contains(relationShip)))
-                .collect(Collectors.toList());
-        if (matchedNodeLists.isEmpty()) {
-            // add nodeAdded, as a list, to nodeListPool if not any nodeList matches
-            nodeListPool.add(new ArrayList<>(Collections.singletonList(nodeAdded)));
-        } else {
-            // otherwise, merge the matchedNodeLists to one list and add the node to the merged list
-            List<Node<DataType>> firstNodeListMatched = matchedNodeLists.get(0);
-            firstNodeListMatched.add(nodeAdded);
-            matchedNodeLists.stream()
-                    .skip(1)
-                    .forEach(nodeList -> {
-                        firstNodeListMatched.addAll(nodeList);
-                        nodeListPool.remove(nodeList);
-                    });
-        }
+        boolean canOptimize = linkableDataTypeOptimize && (nodeToAdd.data() instanceof LinkableDataType);
+        List<Long> matchedLocationList = canOptimize ? optimizedSearchAndLink(nodeToAdd) : searchAndLink(nodeToAdd);
+        modifyContainers(canOptimize, nodeToAdd, matchedLocationList);
     }
 
     public List<Node<DataType>> treeList() {
-        return nodeListPool
+        Map<Long, List<Node<DataType>>> groupList = Stream.concat(
+                linkableLocation4IdKeyed.entrySet().stream().map(Map.Entry::getValue),
+                notLinkableDataNodeLocation.stream())
+                .collect(Collectors.toMap(
+                        Pair::getLeft,
+                        nodePair -> new ArrayList<>(Collections.singletonList(nodePair.getRight())),
+                        (oldValue, newValue) -> {
+                            oldValue.addAll(newValue);
+                            return oldValue;
+                        }));
+        return groupList.entrySet()
                 .stream()
-                .map(nodeList -> {
-                    // won't happen according to the merge rule in add method
-                    if (nodeList.isEmpty()) {
-                        return null;
-                    }
-                    List<Node<DataType>> linkedNodeList = new ArrayList<>(
-                            Collections.singletonList(
-                                    nodeList.get(0)));
-                    nodeList.stream()
-                            // skip first one which already added to linkedNodeList
-                            .skip(1)
-                            // link each node to linkedNodeList
-                            .forEach(node -> {
-                                linkedNodeList.forEach(linkedNode -> {
-                                    if (nodeComparator.isParent(node, linkedNode)) {
-                                        node.addChild(linkedNode);
-                                    }
-                                    if (nodeComparator.isParent(linkedNode, node)) {
-                                        linkedNode.addChild(node);
-                                    }
-                                });
-                                linkedNodeList.add(node);
-                            });
-                    List<Node<DataType>> rootNodeList = linkedNodeList.stream()
+                .map(Map.Entry::getValue)
+                .map(group -> {
+                    List<Node<DataType>> rootNodeList = group.stream()
                             .filter(Node::root)
                             .collect(Collectors.toList());
                     if (rootNodeList.isEmpty()) {
                         throw new RuntimeException(
-                                String.format("no rootNode found in the nodeList(%s)", nodeList));
+                                String.format("no rootNode found in the group(%s)", group));
                     }
                     // won't happen according to the merge rule in add method
                     if (rootNodeList.size() > 1) {
                         throw new RuntimeException(
-                                String.format("more than one root node found in nodeList(%s)", nodeList));
+                                String.format("more than one root node found in group(%s)", group));
                     }
                     return rootNodeList.get(0);
-                }).filter(Objects::nonNull)
+                }).collect(Collectors.toList());
+    }
+
+    private List<Long> searchAndLink(Node<DataType> nodeToAdd) {
+        List<Pair<Long, Node<DataType>>> matchedNodePairList = Stream.concat(Stream.concat(
+                linkableLocation4IdKeyed.entrySet().stream().map(Map.Entry::getValue),
+                linkableLocation4ParentKeyed.entrySet().stream().flatMap(entry -> entry.getValue().stream())),
+                notLinkableDataNodeLocation.stream())
+                .filter(nodePair -> LINKED_RELATION_SHIP.contains(
+                        nodeComparator.relationShip(nodeToAdd, nodePair.getRight())))
                 .collect(Collectors.toList());
+        matchedNodePairList.stream()
+                .map(Pair::getRight)
+                .forEach(relationalNode -> {
+                    switch (nodeComparator.relationShip(nodeToAdd, relationalNode)) {
+                        case PARENT:
+                            nodeToAdd.addChild(relationalNode);
+                            break;
+                        case CHILD:
+                            relationalNode.addChild(nodeToAdd);
+                            break;
+                    }
+                });
+        return matchedNodePairList.stream()
+                .map(Pair::getLeft)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private List<Long> optimizedSearchAndLink(Node<DataType> nodeToAdd) {
+        Pair<Long, Node<DataType>> parentNodePair
+                = linkableLocation4IdKeyed.get(((LinkableDataType) nodeToAdd.data()).parentId());
+        List<Pair<Long, Node<DataType>>> childNodePairList
+                = linkableLocation4ParentKeyed.get(((LinkableDataType) nodeToAdd.data()).id());
+        List<Pair<Long, Node<DataType>>> relationalNodePairList = notLinkableDataNodeLocation
+                .stream()
+                .filter(nodePair
+                        -> LINKED_RELATION_SHIP.contains(
+                        nodeComparator.relationShip(nodeToAdd, nodePair.getRight())))
+                .collect(Collectors.toList());
+        if (null != parentNodePair) {
+            parentNodePair.getRight().addChild(nodeToAdd);
+        }
+        if (null != childNodePairList) {
+            childNodePairList.forEach(childNodePair -> nodeToAdd.addChild(childNodePair.getRight()));
+        }
+        relationalNodePairList.stream()
+                .map(Pair::getRight)
+                .forEach(relationalNode -> {
+                    switch (nodeComparator.relationShip(nodeToAdd, relationalNode)) {
+                        case PARENT:
+                            nodeToAdd.addChild(relationalNode);
+                            break;
+                        case CHILD:
+                            relationalNode.addChild(nodeToAdd);
+                            break;
+                    }
+                });
+        return Stream
+                .concat(
+                        Stream.concat(
+                                null == parentNodePair
+                                        ? Stream.empty() : Stream.of(parentNodePair.getLeft()),
+                                null == childNodePairList
+                                        ? Stream.empty() : childNodePairList.stream().map(Pair::getLeft)),
+                        relationalNodePairList.stream().map(Pair::getLeft))
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private void modifyContainers(boolean canOptimize, Node<DataType> nodeToAdd, List<Long> matchedLocationList) {
+        if (matchedLocationList.isEmpty()) {
+            // add with a new location
+            add(canOptimize, nodeToAdd, locationCounter.getAndIncrement());
+            return;
+        }
+        if (1 == matchedLocationList.size()) {
+            // add to an existing location
+            add(canOptimize, nodeToAdd, matchedLocationList.get(0));
+            return;
+        }
+        // add to an existing location(first) and merge other locations to it
+        long firstMatchLocation = matchedLocationList.get(0);
+        linkableLocation4IdKeyed.entrySet()
+                .forEach(nodePairEntry -> {
+                    Pair<Long, Node<DataType>> nodePair = nodePairEntry.getValue();
+                    if (matchedLocationList.contains(nodePair.getLeft())) {
+                        nodePairEntry.setValue(Pair.of(firstMatchLocation, nodePair.getRight()));
+                    }
+                });
+        linkableLocation4ParentKeyed.entrySet()
+                .forEach(entry
+                        -> entry.setValue(
+                        entry.getValue()
+                                .stream()
+                                .map(childNodePair
+                                        -> matchedLocationList.contains(childNodePair.getLeft())
+                                        ? Pair.of(firstMatchLocation, childNodePair.getRight())
+                                        : childNodePair)
+                                .collect(Collectors.toList())));
+        notLinkableDataNodeLocation = notLinkableDataNodeLocation.stream()
+                .map(nodePair
+                        -> matchedLocationList.contains(nodePair.getLeft())
+                        ? Pair.of(firstMatchLocation, nodePair.getRight())
+                        : nodePair)
+                .collect(Collectors.toList());
+        add(canOptimize, nodeToAdd, firstMatchLocation);
+    }
+
+    private void add(boolean canOptimize, Node<DataType> nodeToAdd, long locationToAdd) {
+        if (canOptimize) {
+            linkableLocation4IdKeyed.put(
+                    ((LinkableDataType) nodeToAdd.data()).id(),
+                    Pair.of(locationToAdd, nodeToAdd));
+            linkableLocation4ParentKeyed.merge(
+                    ((LinkableDataType) nodeToAdd.data()).parentId(),
+                    new ArrayList<>(Collections.singletonList(Pair.of(locationToAdd, nodeToAdd))),
+                    (oldValue, newValue) -> {
+                        oldValue.addAll(newValue);
+                        return oldValue;
+                    });
+        } else {
+            notLinkableDataNodeLocation.add(Pair.of(locationToAdd, nodeToAdd));
+        }
     }
 }
--- tree/src/test/java/tech/geekcity/open/geek/tools/tree/TreeConstructorTest.java
+++ tree/src/test/java/tech/geekcity/open/geek/tools/tree/TreeConstructorTest.java
@@ -1,7 +1,6 @@
 package tech.geekcity.open.geek.tools.tree;
 
 import com.google.common.collect.TreeTraverser;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,11 +14,31 @@ import java.util.stream.StreamSupport;
  * @author ben.wangz
  */
 public class TreeConstructorTest {
-    private Node<String> root;
+    private static class LinkableData implements LinkableDataType {
+        private String id;
+        private String parentId;
+
+        public LinkableData(String id, String parentId) {
+            this.id = id;
+            this.parentId = parentId;
+        }
+
+        @Override
+        public String id() {
+            return id;
+        }
+
+        @Override
+        public String parentId() {
+            return parentId;
+        }
+    }
+
+    private Node<String> stringTreeRoot;
 
     @Before
     public void before() throws Exception {
-        root = new Node<>("h")
+        stringTreeRoot = new Node<>("h")
                 .addChild(
                         new Node<>("d")
                                 .addChild(new Node<>("a"))
@@ -43,16 +62,25 @@ public class TreeConstructorTest {
 
                     @Override
                     public boolean equals(Node<Node<String>> node, Node<Node<String>> anotherNode) {
-                        return StringUtils.equals(
+                        return Objects.equals(
                                 node.data().data(),
                                 anotherNode.data().data());
                     }
                 });
+        TreeConstructor<LinkableData> linkableDataTreeConstructor
+                = new TreeConstructor<>(LinkableDataType.nodeComparator());
+
         TreeTraverser.<Node<String>>using(Node::children)
-                .postOrderTraversal(root)
-                .forEach(node -> treeConstructor.add(new Node<>(node)));
+                .postOrderTraversal(stringTreeRoot)
+                .forEach(node -> {
+                    treeConstructor.add(new Node<>(node));
+                    linkableDataTreeConstructor.add(
+                            new Node<>(new LinkableData(node.data(), node.parent() == null ? "" : node.parent().data())));
+                });
         List<Node<Node<String>>> treeList = treeConstructor.treeList();
+        List<Node<LinkableData>> linkableTreeList = linkableDataTreeConstructor.treeList();
         Assert.assertEquals(1, treeList.size());
+        Assert.assertEquals(1, linkableTreeList.size());
         Assert.assertEquals(
                 StreamSupport.stream(
                         TreeTraverser.<Node<Node<String>>>using(Node::children)
@@ -62,7 +90,20 @@ public class TreeConstructorTest {
                         .collect(Collectors.toList()),
                 StreamSupport.stream(
                         TreeTraverser.<Node<String>>using(Node::children)
-                                .breadthFirstTraversal(root).spliterator(),
+                                .breadthFirstTraversal(stringTreeRoot).spliterator(),
+                        false)
+                        .map(Node::data)
+                        .collect(Collectors.toList()));
+        Assert.assertEquals(
+                StreamSupport.stream(
+                        TreeTraverser.<Node<LinkableData>>using(Node::children)
+                                .breadthFirstTraversal(linkableTreeList.get(0)).spliterator(),
+                        false)
+                        .map(node -> node.data().id())
+                        .collect(Collectors.toList()),
+                StreamSupport.stream(
+                        TreeTraverser.<Node<String>>using(Node::children)
+                                .breadthFirstTraversal(stringTreeRoot).spliterator(),
                         false)
                         .map(Node::data)
                         .collect(Collectors.toList()));
@@ -75,19 +116,37 @@ public class TreeConstructorTest {
                         .collect(Collectors.toList()),
                 StreamSupport.stream(
                         TreeTraverser.<Node<String>>using(Node::children)
-                                .preOrderTraversal(root).spliterator(),
+                                .preOrderTraversal(stringTreeRoot).spliterator(),
+                        false)
+                        .map(Node::data)
+                        .collect(Collectors.toList()));
+        Assert.assertEquals(
+                StreamSupport.stream(
+                        TreeTraverser.<Node<LinkableData>>using(Node::children)
+                                .preOrderTraversal(linkableTreeList.get(0)).spliterator(),
+                        false)
+                        .map(node -> node.data().id())
+                        .collect(Collectors.toList()),
+                StreamSupport.stream(
+                        TreeTraverser.<Node<String>>using(Node::children)
+                                .preOrderTraversal(stringTreeRoot).spliterator(),
                         false)
                         .map(Node::data)
                         .collect(Collectors.toList()));
 
         treeConstructor.clear();
+        linkableDataTreeConstructor.clear();
         StreamSupport.stream(
                 TreeTraverser.<Node<String>>using(Node::children)
-                        .preOrderTraversal(root).spliterator(),
+                        .preOrderTraversal(stringTreeRoot).spliterator(),
                 false)
                 // skip root 'h'
                 .skip(1)
-                .forEach(node -> treeConstructor.add(new Node<>(node)));
+                .forEach(node -> {
+                    treeConstructor.add(new Node<>(node));
+                    linkableDataTreeConstructor.add(new Node<>(
+                            new LinkableData(node.data(), node.parent() == null ? "" : node.parent().data())));
+                });
         Assert.assertEquals(
                 treeConstructor.treeList()
                         .stream()
@@ -100,7 +159,30 @@ public class TreeConstructorTest {
                                         .collect(Collectors.joining()))
                         .sorted()
                         .collect(Collectors.toList()),
-                root.children()
+                stringTreeRoot.children()
+                        .stream()
+                        .map(rootChild ->
+                                StreamSupport.stream(
+                                        TreeTraverser.<Node<String>>using(Node::children)
+                                                .breadthFirstTraversal(rootChild).spliterator(),
+                                        false)
+                                        .map(Node::data)
+                                        .collect(Collectors.joining()))
+                        .sorted()
+                        .collect(Collectors.toList()));
+        Assert.assertEquals(
+                linkableDataTreeConstructor.treeList()
+                        .stream()
+                        .map(tree ->
+                                StreamSupport.stream(
+                                        TreeTraverser.<Node<LinkableData>>using(Node::children)
+                                                .breadthFirstTraversal(tree).spliterator(),
+                                        false)
+                                        .map(node -> node.data().id())
+                                        .collect(Collectors.joining()))
+                        .sorted()
+                        .collect(Collectors.toList()),
+                stringTreeRoot.children()
                         .stream()
                         .map(rootChild ->
                                 StreamSupport.stream(
